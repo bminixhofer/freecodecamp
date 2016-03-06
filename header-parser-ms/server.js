66235d2fe31e2257e04a4f1d328574303ccb350b
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-03-06 12:06:38 +0000

    completed application

--- header-parser-ms/server.js
+++ header-parser-ms/server.js
@@ -1,33 +1,64 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+var months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use(express.static(__dirname + '/public'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.get(/.*/, function(req, res) {
+	var input = req.url.slice(1);
+	
+	if(input.length > 0) {
+		var jsDate = convertFromNaturalDate(input);
+		var date = new Date((jsDate != "Invalid Date") ? jsDate : Number(input));
+		var obj = { unix: null, natural: null };
+	
+		if(date.toString() != "Invalid Date") {
+			obj.unix = date.getTime();
+			obj.natural = months[date.getMonth()] + " " + date.getDate() + ", " + date.getUTCFullYear();
+		}
+			
+		res.end(JSON.stringify(obj));
+	} else {
+		res.end();
+	}
 });
+app.listen(8080);
+
+function convertFromNaturalDate(date) {
+	var matches = date.match(/(\w+)%20(\d+),%20(\d+)/i) || [];
+	if(matches.length == 4) {
+		matches = matches.slice(1);
+		var compareMonths = months.map(function(value) {
+			return value.toLowerCase();	
+		});
+		return addZeros(matches[2], 4) + "-" + addZeros(compareMonths.indexOf(matches[0].toLowerCase()) + 1, 2) + "-" + addZeros(matches[1]); 
+	} else {
+		return "Invalid Date";
+	}
+}
+
+function addZeros(str, num) {
+	for(var i = 0; i < str.length; i++) {
+		if(str.length < num) {
+			str = "0" + str;
+		}
+	}
+	
+	return str;
+}
