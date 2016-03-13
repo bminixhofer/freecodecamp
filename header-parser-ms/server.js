c14cba1f9dfb3c37738e64c457fcb8b93b76ae66
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-03-13 12:54:15 +0000

    cleaned up

--- header-parser-ms/server.js
+++ header-parser-ms/server.js
@@ -2,63 +2,29 @@
 
 var express = require('express');
 var app = express();
-
-var months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get(/.*/, function(req, res) {
-	var input = req.url.slice(1);
-	
-	if(input.length > 0) {
-		var jsDate = convertFromNaturalDate(input);
-		var date = new Date((jsDate != "Invalid Date") ? jsDate : Number(input));
-		var obj = { unix: null, natural: null };
-	
-		if(date.toString() != "Invalid Date") {
-			obj.unix = date.getTime();
-			obj.natural = months[date.getMonth()] + " " + date.getDate() + ", " + date.getUTCFullYear();
-		}
-			
+app.get("/getdata", function(req, res) {
+	var obj = {};
+	callAjax("http://www.trackip.net/ip?json", function(data) {
+		obj.ip = JSON.parse(data).ip;
+		obj.software = /\(([^\)]+)\)/.exec(req.headers['user-agent'])[1];
+		obj.language = req.acceptsLanguages()[0]
 		res.end(JSON.stringify(obj));
-	} else {
-		res.end();
-	}
+	});
 });
 app.listen(8080);
 
-function convertFromNaturalDate(date) {
-	var matches = date.match(/(\w+)%20(\d+),%20(\d+)/i) || [];
-	if(matches.length == 4) {
-		matches = matches.slice(1);
-		var compareMonths = months.map(function(value) {
-			return value.toLowerCase();	
-		});
-		return addZeros(matches[2], 4) + "-" + addZeros(compareMonths.indexOf(matches[0].toLowerCase()) + 1, 2) + "-" + addZeros(matches[1]); 
-	} else {
-		return "Invalid Date";
-	}
-}
-
-function addZeros(str, num) {
-	for(var i = 0; i < str.length; i++) {
-		if(str.length < num) {
-			str = "0" + str;
-		}
-	}
-	
-	return str;
+function callAjax(url, callback){
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
