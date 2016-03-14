64c5d9f69903bff2dbf5f44d915be0efb0e9a5e9
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-03-14 12:35:10 +0000

    initial commit

--- file-metadata-ms/server.js
+++ file-metadata-ms/server.js
@@ -2,63 +2,22 @@
 
 var express = require('express');
 var app = express();
+var bing = require('node-bing-api')({ accKey: "5m1MQD9wHZnCt4cDnIJKGmMAokYFK9irBszcYwFPgzM" });
 
-var months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-
-app.use(express.static(__dirname + '/public'));
-
-app.get(/.*/, function(req, res) {
-	var input = req.url.slice(1);
-	
-	if(input.length > 0) {
-		var jsDate = convertFromNaturalDate(input);
-		var date = new Date((jsDate != "Invalid Date") ? jsDate : Number(input));
-		var obj = { unix: null, natural: null };
-	
-		if(date.toString() != "Invalid Date") {
-			obj.unix = date.getTime();
-			obj.natural = months[date.getMonth()] + " " + date.getDate() + ", " + date.getUTCFullYear();
-		}
-			
-		res.end(JSON.stringify(obj));
-	} else {
-		res.end();
-	}
+app.get("/imagesearch/:search", function(req, res) {
+    bing.images(req.params.search, { top: 5, skip: req.query.offset || 0 }, function(err, resp, body) {
+        if(err) throw err;
+        var response = [];
+        body.d.results.forEach(function(obj) {
+            response.push( {
+                url: obj.MediaUrl,
+                snippet: obj.Title,
+                context: obj.SourceUrl,
+                thumbnail: obj.Thumbnail.MediaUrl
+            });
+        });
+        res.end(JSON.stringify(response));
+    });
 });
-app.listen(8080);
 
-function convertFromNaturalDate(date) {
-	var matches = date.match(/(\w+)%20(\d+),%20(\d+)/i) || [];
-	if(matches.length == 4) {
-		matches = matches.slice(1);
-		var compareMonths = months.map(function(value) {
-			return value.toLowerCase();	
-		});
-		return addZeros(matches[2], 4) + "-" + addZeros(compareMonths.indexOf(matches[0].toLowerCase()) + 1, 2) + "-" + addZeros(matches[1]); 
-	} else {
-		return "Invalid Date";
-	}
-}
-
-function addZeros(str, num) {
-	for(var i = 0; i < str.length; i++) {
-		if(str.length < num) {
-			str = "0" + str;
-		}
-	}
-	
-	return str;
-}
+app.listen(process.env.PORT || 8080);
