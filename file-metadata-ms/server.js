e9d3afcc051e2bb9cc66c9d781f22f8d5ec41367
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-03-15 14:23:34 +0000

    initial commit

--- file-metadata-ms/server.js
+++ file-metadata-ms/server.js
@@ -2,56 +2,16 @@
 
 var express = require('express');
 var app = express();
-var bing = require('node-bing-api')({ accKey: "5m1MQD9wHZnCt4cDnIJKGmMAokYFK9irBszcYwFPgzM" });
-var mongo = require('mongodb').MongoClient;
+var multer = require('multer');
+var autoReap  = require('multer-autoreap');
 
+app.use(multer({dest:'./uploads/'}).single('fileToUpload'));
 app.use(express.static(__dirname + '/public'));
+app.use(autoReap);
 
-app.get("/imagesearch/:search", function(req, res) {
-    var query = req.params.search;
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if(err) throw err;
-        
-        db.collection('recentsearches').insert( {
-            search: query,
-            time: new Date().toDateString()
-        }, function(err, db) {
-            if(err) throw err;
-        });
-    });
-    bing.images(query, { top: 5, skip: req.query.offset || 0 }, function(err, resp, body) {
-        if(err) throw err;
-        var response = [];
-        body.d.results.forEach(function(obj) {
-            response.push( {
-                url: obj.MediaUrl,
-                snippet: obj.Title,
-                context: obj.SourceUrl,
-                thumbnail: obj.Thumbnail.MediaUrl
-            });
-        });
-        res.end(JSON.stringify(response));
-    });
+app.post('/filedata', function(req, res){
+    res.end(JSON.stringify( { name: req.file.originalname, size: req.file.size } ));
 });
 
-app.get("/mostrecent", function(req, res) {
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if(err) throw err;
-        
-        db.collection('recentsearches').find().sort({_id: -1}).limit(10).toArray(function(err, docs) {
-            if(err) throw err;
-            
-            docs = docs.map(function(obj) {
-                return {
-                    search: obj.search,
-                    time: obj.time
-                };
-            });
-            
-            res.end(JSON.stringify(docs));
-        });
-    });
-});
-
-
+app.listen(3000);
 app.listen(process.env.PORT || 8080);
