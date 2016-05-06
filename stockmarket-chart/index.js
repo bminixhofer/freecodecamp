75c4778eee8468f7a46cd4c7eb81479a1b4c4f22
bminixhofer <bj.minixhofer@gmail.com>
2016-05-06 20:38:40 +0200

    added socket.io client + minor changes

--- stockmarket-chart/index.js
+++ stockmarket-chart/index.js
@@ -3,8 +3,16 @@ var http = require('http');
 var io = require('socket.io')(http);
 
 var app = express();
+var stocks = [];
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+  socket.on('stock add', function(code) {
+    stocks.push(code);
+    console.log(stocks);
+  });
+});
 
 app.listen('8080');
 console.log('Started..');
