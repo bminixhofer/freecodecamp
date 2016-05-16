5fc1155e97a5c8bf303f9454f438177ef35b8a38
bminixhofer <bj.minixhofer@gmail.com>
2016-05-16 22:53:35 +0200

    changed the way files are served

--- trade-books/index.js
+++ trade-books/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var mongo = require('mongodb').MongoClient;
 var app = express();
 
+app.get('/', function(req, res) {
+  console.log('req');
+  res.sendFile(__dirname + '/pages/index.html');
+});
+
 app.use(express.static(__dirname + '/public'));
+
+
 app.listen(process.env.PORT || '8080');
 console.log('Started..');
