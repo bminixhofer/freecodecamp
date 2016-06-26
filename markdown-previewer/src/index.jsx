714ba40ab20791fa48661e4ed99e7069ba9e44fc
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-06-26 11:39:27 +0200

    added styling and finished functionality

--- markdown-previewer/src/index.jsx
+++ markdown-previewer/src/index.jsx
@@ -1,4 +1,69 @@
+var Input = React.createClass({
+  textChanged: function() {
+    var text = document.querySelector('.input > textarea');
+    text.value = text.value.replace(/\\n/g, '\n');
+    this.props.onChange(text);
+  },
+  componentDidMount: function() {
+    this.textChanged();
+  },
+  render: function() {
+    return (
+      <div className="input">
+        <textarea type="text" onLoad={this.textChanged} onChange={this.textChanged} defaultValue="
+        Heading
+        =======
+        \n
+        Sub-heading
+        -----------
+        \n
+        ### Another deeper heading
+        \n
+        Paragraphs are separated by a blank line.
+        \n
+        Leave 2 spaces at the end of a line to do a
+        line break
+        \n
+        Text attributes *italic*, **bold**,
+        `monospace`, ~~strikethrough~~ .
+        \n
+        *[Benjamin Minixhofer](https://github.com/bminixhofer)*"/>
+      </div>
+    );
+  }
+});
+
+var Output = React.createClass({
+  render: function() {
+    return (
+      <div className="output" dangerouslySetInnerHTML={this.props.text}></div>
+    );
+  }
+});
+
+var Previewer = React.createClass({
+  getInitialState: function() {
+    return {text: ``};
+  },
+  setText: function(e) {
+    var md = new Remarkable();
+    var markdown = md.render(e.value.toString());
+    this.setState({text: markdown});
+  },
+  render: function() {
+    return (
+      <div className="previewer">
+        <Input onChange={this.setText}/>
+        <Output text={{__html: this.state.text}}/>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <div>
+    <h1 className="title">Markdown Previewer</h1>
+    <Previewer />
+  </div>,
   document.getElementById('app')
 )
