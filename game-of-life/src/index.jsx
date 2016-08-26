c6299cea1f5a7bb9208f5c175ea0bf650b1f172e
bminixhofer <bj.minixhofer@gmail.com>
2016-08-26 12:15:16 +0100

    finished styling, debugged code

--- game-of-life/src/index.jsx
+++ game-of-life/src/index.jsx
@@ -1,170 +1,152 @@
-function generateKey(name) {
-  var key = Date.now() + name;
-  for(var i = 0; i < 8; i++) {
-    key += Math.floor(Math.random() * 10);
-  }
-  return key;
-}
-
-var Popup = React.createClass({
-  sendData: function(e) {
-    var parent = $(e.target.parentNode);
-    var data = {
-      name: parent.children('input[name=Name]').val(),
-      ingredients: parent.children('input[name=Ingredients]').val().split(/,\s?/)
-    }
-    if(this.props.defaultData) {
-      data.key = this.props.defaultData.key;
-    }
-    if(data.name && data.ingredients) {
-      this.props.onSend(data);
-      this.props.changeState(false);
-    }
-  },
+var Field = React.createClass({
   render: function() {
-    var data = this.props.defaultData;
-    return (
-      <div className="popup" style={{display: this.props.display ? 'block' : 'none'}}>
-        <h2>Name</h2><br />
-        <input defaultValue={data ? data.name : ""} type="text" name="Name" /><br />
-        <h2>Ingredients</h2><p>separated by a comma</p><br />
-        <input defaultValue={data ? data.ingredients.join(', ') : ""}type="text" name="Ingredients" /><br />
-        <input onClick={this.sendData} type="button" value="Submit" />
-      </div>
-    );
-  }
-});
-
-var ListElement = React.createClass({
-  getInitialState: function() {
-    return {
-      display: false,
-    }
-  },
-  setPopupState: function(state) {
-    this.setState({
-      display: state
-    });
-  },
-  render: function() {
-    var ingredients = this.props.data.ingredients.map(function(ingredient) {
-      return (
-        <p key={generateKey(ingredient)}>{ingredient}</p>
-      );
-    });
-    return (
-      <div className="recipe">
-        <h3 onClick={this.props.setDisplay.bind(null, this.props.data.key)}>{this.props.data.name}</h3>
-        <div style={{display: this.props.displayed === this.props.data.key ? 'block' : 'none'}} className="info">
-          <h2>{this.props.data.name}</h2>
-          {ingredients}
-          <input name="Edit" type="button" value="Edit" onClick={this.setPopupState.bind(null, true)}/>
-          <input name="Delete" type="button" value="Delete" onClick={this.props.onDelete.bind(null, this.props.data.key)}/>
-        </div>
-        <Popup changeState={this.setPopupState} display={this.state.display} defaultData={this.props.data} onSend={this.props.onChange}/>
-      </div>
-    );
-  }
-});
-
-var List = React.createClass({
-  getInitialState: function() {
-    return {
-      displayed: ""
-    }
-  },
-  setDisplay: function(key) {
-    this.setState({
-      displayed: this.state.displayed === key ? "" : key
-    });
-  },
-  render: function() {
+    var y = 0;
     var _this = this;
-    var nodes = this.props.data.map(function(node) {
-      return (
-        <ListElement setDisplay={_this.setDisplay} displayed={_this.state.displayed} onDelete={_this.props.onDelete} onChange={_this.props.onChange} key={node.key} data={node} />
-      );
-    });
+    var board = this.props.board.reduce(function(a, b) {
+      b = b.map(function(element, x) {
+        return (
+          <div onClick={_this.props.setCell.bind(null, x, y)} className={"cell " + (element === 1 ? 'alive' : 'dead')}>
+          </div>
+        );
+      });
+      b.push((
+        <br />
+      ));
+      y++;
+      return b.concat(a);
+    }, []);
     return (
-      <div className="container">
-        {nodes}
+      <div className="field">
+        {board}
       </div>
     );
   }
 });
 
-var AddButton = React.createClass({
+var Game = React.createClass({
   getInitialState: function() {
-    return {
-      display: false
+    var state =  {
+      options: {
+        speed: 3,
+        size: 10
+      },
+      fromUpdate: 0,
+      board: [],
+      runState: 'Run'
+    }
+    state.board = this.generateBoard(state.options);
+    setInterval(this.updateBoard, 200);
+    return state;
+  },
+  updateBoard: function(count) {
+    this.state.fromUpdate++;
+    var _this = this;
+    var board = JSON.parse(JSON.stringify(this.state.board));
+    if(this.state.fromUpdate >= this.state.options.speed && this.state.runState === 'Run') {
+      board.forEach(function(row, y) {
+        row.forEach(function(_, x) {
+          board[x][y] = takeTurn(board, x, y);
+        });
+      });
+      this.setState({
+        fromUpdate: 0,
+        board: board
+      });
+    }
+    function takeTurn(board, x, y) {
+      var neighbourCount = 0;
+      neighbourCount +=
+        getPos(board, x + 1, y) +
+        getPos(board, x - 1, y) +
+        getPos(board, x, y - 1) +
+        getPos(board, x, y + 1) +
+        getPos(board, x + 1, y + 1) +
+        getPos(board, x - 1, y - 1) +
+        getPos(board, x + 1, y - 1) +
+        getPos(board, x - 1, y + 1);
+
+      if(neighbourCount < 2 || neighbourCount > 3) {
+        return 0;
+      }
+      if(neighbourCount === 3) {
+        return 1;
+      }
+      return board[x][y];
+    }
+    function getPos(board, x, y) {
+      return board[x] ? (board[x][y] || 0) : 0;
     }
   },
-  dataIsSent: function(data) {
-    this.props.onAdd(data);
+  generateBoard: function(options) {
+    var board = [];
+    for(var i = 0; i < options.size; i++) {
+      var column = [];
+      for(var j = 0; j < options.size; j++) {
+        column.push(Math.round(Math.random() * 10) <= 3 ? 1 : 0);
+      }
+      board.push(column);
+    }
+    return board;
   },
-  setPopupState: function(state) {
-    this.setState({display: state});
+  clearBoard: function() {
+    var board = this.state.board;
+    board = board.map(function(row) {
+      return row.map(function() {
+        return 0;
+      })
+    });
+    this.setState({
+      board: board
+    });
+  },
+  changeSize: function(e) {
+    var options = this.state.options;
+    options.size = e.target.dataset.size;
+    this.setState({
+      options: options,
+      board: this.generateBoard(options)
+    });
+  },
+  changeSpeed: function(e) {
+    this.setState({
+      options: {
+        speed: e.target.dataset.speed
+      }
+    });
+  },
+  toggleRunState: function() {
+    this.setState({
+      runState: this.state.runState === 'Run' ? 'Pause' : 'Run'
+    });
+  },
+  setCell: function(x, y) {
+    var board = this.state.board;
+    board[y][x] = 1;
+    this.setState({
+      board: board
+    });
   },
   render: function() {
     return (
-      <div>
-        <input className="addButton" onClick={this.setPopupState.bind(null, true)} type="button" value="Add" />
-        <Popup changeState={this.setPopupState} onSend={this.dataIsSent} display={this.state.display}/>
-      </div>
-    );
-  }
-});
+      <div className="game">
+        <div className="headline">
+          <input onClick={this.toggleRunState} type="button" value={this.state.runState === 'Run' ? 'Pause' : 'Run'}></input>
+          <input onClick={this.clearBoard} type="button" value="Clear"></input>
+        </div>
+        <Field setCell={this.setCell} board={this.state.board}/>
 
-var RecipeBox = React.createClass({
-  getInitialState: function() {
-    var prev = localStorage.getItem('recipes');
-    return {
-      recipes: prev ? JSON.parse(prev) : []
-    }
-  },
-  addItem: function(item) {
-    item.key = generateKey(item.name);
-    var recipes = JSON.parse(JSON.stringify(this.state.recipes));
-    recipes.push(item);
-
-    this.setState({
-      recipes: recipes
-    });
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-  },
-  changeItem: function(item) {
-    var recipes = JSON.parse(JSON.stringify(this.state.recipes));
-    recipes = recipes.map(function(recipe) {
-      if(recipe.key === item.key) {
-        return item;
-      }
-      return recipe;
-    });
-
-    this.setState({
-      recipes: recipes
-    });
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-  },
-  deleteItem: function(key) {
-    var recipes = JSON.parse(JSON.stringify(this.state.recipes));
-    recipes.forEach(function(recipe, index) {
-      if(recipe.key === key) {
-        recipes.splice(index, 1);
-        return;
-      }
-    });
-
-    this.setState({
-      recipes: recipes
-    });
-    localStorage.setItem('recipes', JSON.stringify(recipes));
-  },
-  render: function() {
-    return (
-      <div>
-        <List data={this.state.recipes} onDelete={this.deleteItem} onChange={this.changeItem}/>
-        <AddButton onAdd={this.addItem}/>
+        <div className="settings">
+          <div className="size">
+            <input type="radio" name="size" data-size={10} onChange={this.changeSize} defaultChecked/>10x10<br />
+            <input type="radio" name="size" data-size={20} onChange={this.changeSize}/>20x20<br />
+            <input type="radio" name="size" data-size={30} onChange={this.changeSize}/>30x30<br />
+          </div>
+          <div className="speed">
+            Slow<input type="radio" name="speed" data-speed={5} onChange={this.changeSpeed}/><br />
+            Normal<input type="radio" name="speed" data-speed={3} onChange={this.changeSpeed} defaultChecked/><br />
+            Fast<input type="radio" name="speed" data-speed={1} onChange={this.changeSpeed}/><br />
+          </div>
+        </div>
       </div>
     );
   }
@@ -172,7 +154,6 @@ var RecipeBox = React.createClass({
 
 ReactDOM.render(
   <div>
-    <h1>Recipe Box</h1>
-    <RecipeBox />
+    <Game />
   </div>, document.getElementById('app')
 );
