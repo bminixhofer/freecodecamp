815f2d0c3b2fc0ef9f10ac176c6d925e7394ae97
bminixhofer <bj.minixhofer@gmail.com>
2016-10-03 13:42:28 +0200

    removed hardcoded values

--- fcc-heatmap/index.js
+++ fcc-heatmap/index.js
@@ -1,7 +1,13 @@
 const dataSource = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 const height = 650,
   barHeight = 50,
-  width = 1400;
+  width = 1400,
+  legendHeight = 30,
+  legendWidth = 400;
+const margin = {
+  vertical: 100,
+  horizontal: 50
+};
 const months = [
   'January',
   'February',
@@ -22,32 +28,32 @@ let chart = d3.select(".chart")
   .attr("width", width);
 
 let gradient = chart.append("defs").append("linearGradient")
-  .attr("id", "linear-gradient")	  
+  .attr("id", "linear-gradient")
   .attr("x1", "0%")
   .attr("y1", "0%")
   .attr("x2", "100%")
-  .attr("y2", "0%");  
+  .attr("y2", "0%");
 
 for(let i = 0; i < 1; i += 0.1) {
   gradient.append("stop")
-  .attr("offset", `${i * 100}%`)   
+  .attr("offset", `${i * 100}%`)
   .attr("stop-color", d3.interpolateSpectral(1 - i));
 }
 
 let legend = chart.append("g")
   .attr("transform", `translate(${width - 400}, ${height - 60})`);
 legend.append("rect")
-  .attr("width", 300)
-  .attr("height", 20)
+  .attr("width", legendWidth)
+  .attr("height", legendHeight)
   .style("fill", "url(#linear-gradient)");
 
 
 let color = d3.scaleSequential(d3.interpolateSpectral);
 let y = d3.scaleLinear()
-  .range([0, height - 100])
+  .range([0, height - margin.vertical])
   .domain([1, 13]);
 let x = d3.scaleLinear()
-  .range([0, width - 50]);
+  .range([0, width - margin.horizontal]);
 
 let xAxis = d3.axisBottom();
 let yAxis = d3.axisLeft()
@@ -71,7 +77,7 @@ chart.call(tip);
 d3.json(dataSource, (err, json) => {
   if(err) throw err;
 
-  let data = json.monthlyVariance;  
+  let data = json.monthlyVariance;
 
   let variance = data.map(d => d.variance);
   color.domain(d3.extent(variance).reverse());
@@ -79,14 +85,15 @@ d3.json(dataSource, (err, json) => {
   xAxis.scale(x);
 
   for(let i = 0; i <= 1; i += 0.2) {
+    let temp = variance[Math.floor((variance.length - 1) * i)] + json.baseTemperature;
     legend.append("text")
-      .text(variance[Math.floor((variance.length - 1) * i)])
-      .attr("transform", `translate(${270 * i}, 35)`)
+      .text(Math.round(temp * 100) / 100)
+      .attr("transform", `translate(${legendWidth * 0.95 * i}, ${legendHeight * 1.5})`)
       .attr("font-size", "12px");
   }
-   
+
   chart.append("g")
-    .attr("transform", `translate(0, ${height - 96})`)
+    .attr("transform", `translate(0, ${height - margin.vertical + 4})`)
     .call(xAxis);
 
   let bar = chart.append("g").selectAll("g")
@@ -98,5 +105,5 @@ d3.json(dataSource, (err, json) => {
     .attr("height", barHeight)
     .attr("width", Math.ceil(width / (data.length / 12)))
     .attr("fill", (d, i) => color(d.variance));
-	
+
 });
