d4fa58acd6a2ba682cfb11a143492b63c62171de
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-07 21:01:47 +0200

    fixed a semicolon

--- fcc-scatterplot/index.js
+++ fcc-scatterplot/index.js
@@ -1,7 +1,7 @@
 const dataSource = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 const height = 600,
   width = 1400,
-  textMargin = 3;
+  textMargin = 3,
   heightMargin = -1,
   pointSize = 10;
 
