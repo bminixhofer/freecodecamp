e4dcf01d558a544521cbc954ca77d10f15b864db
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-08 18:44:51 +0200

    fixed data source

--- fcc-meteoritemap/index.js
+++ fcc-meteoritemap/index.js
@@ -1,4 +1,4 @@
-const dataSource = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+const dataSource = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json";
 const height = window.innerHeight,
   width = window.innerWidth;
 
