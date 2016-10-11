6ab94c3fdb590d6aad30d6b9fd177d88b91fe4d7
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-11 19:06:58 +0200

    added basic serverside

--- simplerest/lib/login.js
+++ simplerest/lib/login.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const Twitter = require("node-twitter-api");
-const secret = require("../twitter-api.js");
+const Twitter = require('node-twitter-api');
+const secret = require('../twitter-api.js');
 
-module.exports = function(app) {
+module.exports = function login(app) {
   let _requestSecret;
   let _requestToken;
 
-  app.get("/start-login", function(req, res) {
-    twitter.getRequestToken(function(err, requestToken, requestSecret) {
+  app.get('/start-login', (req, res) => {
+    twitter.getRequestToken((err, requestToken, requestSecret) => {
       if (err) res.status(500).send(err);
 
       _requestSecret = requestSecret;
@@ -17,16 +17,17 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/confirm-login", function(req, res) {
-    twitter.getAccessToken(_requestToken, _requestSecret, req.query.oauth_verifier, function(err, accessToken, accessTokenSecret, results) {
+  app.get('/confirm-login', (req, res) => {
+    twitter.getAccessToken(_requestToken, _requestSecret, req.query.oauth_verifier, (err, accessToken, accessTokenSecret, results) => {
       if (err) {
         console.log(err);
       } else {
-        twitter.verifyCredentials(accessToken, accessTokenSecret, {}, function(err, data, response) {
+        twitter.verifyCredentials(accessToken, accessTokenSecret, {}, (err, data) => {
           if (err) {
             console.log(err);
           } else {
-            res.end(JSON.stringify(data));
+            res.cookie('name', data.screen_name);
+            res.redirect('/');
           }
       });
       }
--- simplerest/server.js
+++ simplerest/server.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const login = require('./lib/login.js');
+const mongo = require('mongodb').MongoClient;
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
+const mongodbURI = 'mongodb://localhost:27017';
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
+
 login(app);
 
-app.listen(8080);
+app.get('/api/entry', (req, res) => {
+  mongo.connect(mongodbURI, (err, db) => {
+    if(err) throw err;
+
+    let posts = db.collection('posts');
+    res.end(posts);
+  })
+});
+
+app.put('/api/entry', (req, res) => {
+  if(req.cookies['name']) {
+    mongo.connect(mongodbURI, (err, db) => {
+      if(err) throw err;
+
+      let posts = db.collection('posts');
+      posts.insert({
+        image: req.body.image,
+        description: req.body.description,
+        user: req.cookies['name']
+      });
+    });
+  } else {
+    res.end({
+      error: 'Not logged in.'
+    });
+  }
+});
+
+app.listen(8080); 
 console.log('started');
