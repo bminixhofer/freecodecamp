730dec795dbbfc8edafcac13a7e71f67de61c655
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-16 13:42:45 +0200

    added entry page design

--- simplerest/public/entries.js
+++ simplerest/public/entries.js
@@ -4,6 +4,36 @@ var grid = $('.grid').masonry({
   columnWidth: '.grid-sizer'
 });
 
-grid.imagesLoaded().progress( function() {
+grid.imagesLoaded().progress(function() {
   grid.masonry();
+});
+
+$(document).ready(function() {
+  $("#add-entry").click(function() {
+    $.ajax({
+      url: '/api/entry',
+      type: 'PUT',
+      data: {
+        image: $("#source-input").val(),
+        description: $("#description-input").val()
+      },
+      success: function(result) {
+        location.reload();
+      }
+    });
+  });
+
+  $(".delete-icon").click(function() {
+    var element = this.parentNode;
+    $.ajax({
+      url: '/api/entry',
+      type: 'DELETE',
+      data: {
+        id: this.dataset.id
+      },
+      success: function(response) {
+        grid.masonry('remove', element);
+      }
+    });
+  });
 });
