4a859df461e2084891fd3b638f6ca48f80152a02
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-16 22:23:38 +0200

    transferred grid item template to server.js, added broken image handling

--- simplerest/public/entries.js
+++ simplerest/public/entries.js
@@ -12,28 +12,39 @@ $(document).ready(function() {
   $("#add-entry").click(function() {
     $.ajax({
       url: '/api/entry',
+      dataType: 'json',
       type: 'PUT',
       data: {
         image: $("#source-input").val(),
         description: $("#description-input").val()
       },
-      success: function(result) {
-        location.reload();
+      success: function(response) {
+        var item = $(response.html);
+        grid.append(item).masonry('appended', item);
+        $(".delete-icon[data-id=" + response.id + "]").click(remove);
+        replaceInvalid('img:last');
       }
     });
   });
 
-  $(".delete-icon").click(function() {
-    var element = this.parentNode;
-    $.ajax({
-      url: '/api/entry',
-      type: 'DELETE',
-      data: {
-        id: this.dataset.id
-      },
-      success: function(response) {
-        grid.masonry('remove', element);
-      }
-    });
-  });
+  replaceInvalid('img');
+  $(".delete-icon").click(remove);
 });
+
+function remove() {
+  var element = this.parentNode;
+  $.ajax({
+    url: '/api/entry',
+    type: 'DELETE',
+    data: {
+      id: this.dataset.id
+    },
+    success: function(response) {
+      grid.masonry('remove', element).masonry('layout');
+    }
+  });
+}
+
+function replaceInvalid(query) {
+  $(query).one('error', function() { this.src = '/resources/placeholder.png'; });
+}
