c2d0bf6f41be74f90667ce1295efc695b829536f
Benjamin Minixhofer <bj.minixhofer@gmail.com>
2016-10-23 09:00:00 +0200

    restructured, added serverside error messages

--- simplerest/lib/login.js
+++ simplerest/lib/login.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const Twitter = require('node-twitter-api');
+const twitter = new Twitter({
+  consumerKey: process.env.CONSUMER_KEY,
+  consumerSecret: process.env.CONSUMER_SECRET,
+  callback: 'http://fcc-simplerest.herokuapp.com/confirm-login'
+});
 
 module.exports = function login(app) {
   let _requestSecret;
@@ -8,18 +13,23 @@ module.exports = function login(app) {
 
   app.get('/start-login', (req, res) => {
     twitter.getRequestToken((err, requestToken, requestSecret) => {
-      if (err) res.status(500).send(err);
-
-      _requestSecret = requestSecret;
-      _requestToken = requestToken;
-      res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+      if (err) {
+        console.log('Error getting request token.');
+        res.status(500).send(err);
+      } else {
+        _requestSecret = requestSecret;
+        _requestToken = requestToken;
+        res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+      }
     });
   });
 
   app.get('/confirm-login', (req, res) => {
+    console.log('confirming');
     twitter.getAccessToken(_requestToken, _requestSecret, req.query.oauth_verifier, (err, accessToken, accessTokenSecret, results) => {
       if (err) {
-        console.log(err);
+        console.log('Error getting access token.');
+        res.status(500).send(err);
       } else {
         twitter.verifyCredentials(accessToken, accessTokenSecret, {}, (err, data) => {
           if (err) {
@@ -28,15 +38,8 @@ module.exports = function login(app) {
             res.cookie('name', data.screen_name);
             res.redirect('/entries');
           }
-      });
+        });
       }
     });
-  });
-  console.log(process.env.CONSUMER_SECRET);
-  console.log(process.env.CONSUMER_KEY);
-  const twitter = new Twitter({
-    consumerKey: process.env.CONSUMER_KEY,
-    consumerSecret: process.env.CONSUMER_SECRET,
-    callback: 'fcc-simplerest.herokuapp.com/confirm-login'
   });
 };
